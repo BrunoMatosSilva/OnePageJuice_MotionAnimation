10a06cc962e135ccb8e35a8be3aec5f5c427440e
Bruno Matos Silva <bmsfrontend@gmail.com>
2021-11-11 09:43:03 -0300

    Initial commit

---
+++ src/components/Leaf.tsx
@@ -0,0 +1,17 @@
+import { motion } from "framer-motion";
+import { leaf, leafWrapper } from "animations";
+
+const Leaf = ({ className, imageUrl, animationSpeed }) => {
+  return (
+    <motion.div variants={leafWrapper} className={className}>
+      <motion.img
+        custom={animationSpeed}
+        variants={leaf}
+        className="leaf"
+        src={imageUrl}
+      />
+    </motion.div>
+  );
+};
+
+export default Leaf;
